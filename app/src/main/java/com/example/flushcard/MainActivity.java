5a7a9c3cdff48408c62fb58778cfb4d782ae6438
hirossan4049 <haruto405329@gmail.com>
2019-05-26 11:37:20 +0900

    Initial commit

---
+++ app/src/main/java/com/example/flushcard/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.flushcard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Word apple;
+    LinearLayout container;
+    TextView nameTextView;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+                                        //apple_icon
+        apple = new Word(R.drawable.ic_launcher_background,"りんご");
+        container = (LinearLayout)findViewById(R.id.container);
+
+        nameTextView = new TextView(getApplicationContext());
+        nameTextView.setText(apple.name);
+        container.addView(nameTextView);
+
+        LinearLayout layout = new LinearLayout(this.getApplicationContext());
+
+        layout.setOrientation(LinearLayout.HORIZONTAL);
+
+    }
+}
---
+++ app/src/main/java/com/example/flushcard/Word.java
@@ -0,0 +1,14 @@
+package com.example.flushcard;
+
+public class Word {
+
+    int resId;
+
+    String name;
+
+    Word(int resId,String name){
+        this.resId = resId;
+        this.name = name;
+
+    }
+}
