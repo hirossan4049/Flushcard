a5ef523c7e5f745dc217b07f0b18fda85b7b614d
hirossan4049 <haruto405329@gmail.com>
2019-05-26 16:34:43 +0900

    add banana(error

--- app/src/main/java/com/example/flushcard/MainActivity.java
+++ app/src/main/java/com/example/flushcard/MainActivity.java
@@ -8,9 +8,12 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    Word apple;
+    Word apple = new Word(R.drawable.apple_two,"りんご");
+    Word banana = new Word(R.drawable.ic_launcher_background,"ばなな");
+
     LinearLayout container;
     TextView nameTextView;
+    TextView nameTextView1;
 
 
 
@@ -19,25 +22,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
                                         //apple_icon
-        apple = new Word(R.drawable.apple_two,"りんご");
         container = (LinearLayout)findViewById(R.id.container);
 
         nameTextView = new TextView(getApplicationContext());
         nameTextView.setText(apple.name);
-//        container.addView(nameTextView);
+        nameTextView1.setText(banana.name);
 
         LinearLayout layout = new LinearLayout(this.getApplicationContext());
-
         layout.setOrientation(LinearLayout.HORIZONTAL);
-
         ImageView imageView = new ImageView(this);
-
         imageView.setImageResource(apple.resId);
 
         layout.addView(imageView);
         layout.addView(nameTextView);
-
         container.addView(layout);
+
+
+
+        LinearLayout layout1 = new LinearLayout(this.getApplicationContext());
+        layout1.setOrientation(LinearLayout.HORIZONTAL);
+        ImageView imageView1 = new ImageView(this);
+        imageView1.setImageResource(banana.resId);
+
+        layout1.addView(imageView1);
+        layout.addView(nameTextView1);
+        container.addView(layout1);
 
 
     }
